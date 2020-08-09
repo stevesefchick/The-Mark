5a6148c82faa80fb82549a803e97caf0b6f65588
Steve Sefchick <stevesefchick@gmail.com>
2020-08-09 11:02:45 -0400

    Implemented Camera controls

--- Camera.cs
+++ Camera.cs
@@ -1,19 +1,46 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace The_Mark
 {
     class Camera
     {
+		public Vector2 cameraPosition;
 
+		public Camera()
+        {
+			cameraPosition = Vector2.Zero;
+        }
 
-		public void Update(GameTime gameTime)
+		public void Update(Boolean up,Boolean down,Boolean left, Boolean right)
 		{
+            if (up==true)
+            {
+                cameraPosition.Y -= 1;
+            }
+            if (down==true)
+            {
+                cameraPosition.Y += 1;
+            }
+            if (left==true)
+            {
+                cameraPosition.X -= 1;
+            }
+            if (right==true)
+            {
+                cameraPosition.X += 1;
+            }
 		}
 
-		public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-		{
-
-		}
-	}
+        public Matrix get_transformation(GraphicsDeviceManager graphicsDevice)
+        {
+            Matrix _transform =       // Thanks to o KB o for this solution
+              Matrix.CreateTranslation(new Vector3(-cameraPosition.X, -cameraPosition.Y, 0)) *
+                                         Matrix.CreateRotationZ(0) *
+                                         Matrix.CreateScale(new Vector3(1, 1, 1)) *
+                                         Matrix.CreateTranslation(new Vector3(graphicsDevice.PreferredBackBufferWidth * 0.5f, graphicsDevice.PreferredBackBufferHeight * 0.5f, 0));
+            return _transform;
+        }
+    }
 }
--- Main.cs
+++ Main.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using The_Mark;
 
 class GameMain : Game
 {
 	//basic stuff
 	protected SpriteBatch spriteBatch;
+	protected GraphicsDeviceManager gdm;
+
 
 	//content
 	protected WorldMap worldMap;
 	public DataManager dataManager;
 	protected Camera camera;
+
+	//controls
+	protected Boolean isRightPressed;
+	protected Boolean isLeftPressed;
+	protected Boolean isUpPressed;
+	protected Boolean isDownPressed;
+	
 
 	//run the game
 	static void Main(string[] args)
@@ -23,7 +34,7 @@ class GameMain : Game
 
 	protected GameMain()
 	{
-		GraphicsDeviceManager gdm = new GraphicsDeviceManager(this);
+		gdm = new GraphicsDeviceManager(this);
 
 		// All content loaded will be in a "Content" folder
 		Content.RootDirectory = "Content";
@@ -71,8 +82,56 @@ class GameMain : Game
 		base.UnloadContent();
 	}
 
+	protected void getInput()
+    {
+		KeyboardState kbState = Keyboard.GetState();
+
+		//right
+		if (kbState.IsKeyDown(Keys.Right)==true)
+        {
+			isRightPressed = true;
+        }
+		else
+        {
+			isRightPressed = false;
+        }
+
+		//left
+		if (kbState.IsKeyDown(Keys.Left) == true)
+		{
+			isLeftPressed = true;
+		}
+		else
+		{
+			isLeftPressed = false;
+		}
+
+		//up
+		if (kbState.IsKeyDown(Keys.Up) == true)
+		{
+			isUpPressed = true;
+		}
+		else
+		{
+			isUpPressed = false;
+		}
+
+		//down
+		if (kbState.IsKeyDown(Keys.Down) == true)
+		{
+			isDownPressed = true;
+		}
+		else
+		{
+			isDownPressed = false;
+		}
+
+	}
+
 	protected override void Update(GameTime gameTime)
 	{
+		getInput();
+		camera.Update(isUpPressed, isDownPressed, isLeftPressed, isRightPressed);
 		base.Update(gameTime);
 	}
 
@@ -81,7 +140,13 @@ class GameMain : Game
 		GraphicsDevice.Clear(Color.CornflowerBlue);
 
 
-		spriteBatch.Begin();
+		spriteBatch.Begin(SpriteSortMode.BackToFront,
+			BlendState.AlphaBlend,
+			null,
+			null,
+			null,
+			null,
+			camera.get_transformation(gdm));
 		worldMap.Draw(spriteBatch);
 		spriteBatch.End();
 
