a61e114094ab0716298d3fdaa9942e8f13ec4821
Steve Sefchick <stevesefchick@gmail.com>
2020-08-30 13:12:59 -0400

    created road starting/ending positions

---
+++ Road.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace The_Mark
+{
+    class Road
+    {
+        //position info
+        Vector2 startingPosition;
+        Vector2 endingPosition;
+
+
+        //sprites
+
+
+        public Road()
+        {
+
+
+        }
+
+        public void Update()
+        {
+
+
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+
+
+        }
+
+
+
+
+    }
+}
