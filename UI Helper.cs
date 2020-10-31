5c9177ad0ab7f7e063cba66c049f7a3c2cc95059
Steve Sefchick <stevesefchick@gmail.com>
2020-10-31 15:32:34 -0400

    Added borders to hoverCard draw code

--- UI Helper.cs
+++ UI Helper.cs
@@ -15,6 +15,14 @@ namespace The_Mark
         Texture2D hoverCardSprite;
         List<Rectangle> hoverCardLocations = new List<Rectangle>();
         Rectangle hoverCardFillRect = new Rectangle(10, 10, 10, 10);
+        Rectangle hoverCardTopLeft = new Rectangle(0, 0, 10, 10);
+        Rectangle hoverCardTopRight = new Rectangle(20, 0, 10, 10);
+        Rectangle hoverCardBottomLeft = new Rectangle(0, 20, 10, 10);
+        Rectangle hoverCardBottomRight = new Rectangle(20, 20, 10, 10);
+        Rectangle hoverCardTopBorder = new Rectangle(10, 0, 10, 10);
+        Rectangle hoverCardBottomBorder = new Rectangle(10, 20, 10, 10);
+        Rectangle hoverCardLeftBorder = new Rectangle(0, 10, 10, 10);
+        Rectangle hoverCardRightBorder = new Rectangle(20, 10, 10, 10);
 
         public UI_Helper(GameMain thegame)
         {
@@ -36,18 +44,29 @@ namespace The_Mark
                 spriteBatch.Draw(hoverCardSprite, hoverCardLocations[i], hoverCardFillRect, Color.White);
 
                 //draw top left
-
-                //draw top border
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X-10, hoverCardLocations[i].Y-10,10,10), hoverCardTopLeft, Color.White);
 
                 //draw top right
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X + hoverCardLocations[i].Width, hoverCardLocations[i].Y - 10, 10, 10), hoverCardTopRight, Color.White);
 
                 //draw bottom left
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X - 10, hoverCardLocations[i].Y + hoverCardLocations[i].Height, 10, 10), hoverCardBottomLeft, Color.White);
 
                 //draw bottom right
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X + hoverCardLocations[i].Width, hoverCardLocations[i].Y + hoverCardLocations[i].Height, 10, 10), hoverCardBottomRight, Color.White);
+
+                //draw top border
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X, hoverCardLocations[i].Y - 10, hoverCardLocations[i].Width, 10), hoverCardTopBorder, Color.White);
+
+                //draw bottom border
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X, hoverCardLocations[i].Y + hoverCardLocations[i].Height, hoverCardLocations[i].Width, 10), hoverCardBottomBorder, Color.White);
 
                 //draw left border
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X - 10, hoverCardLocations[i].Y, 10, hoverCardLocations[i].Height), hoverCardLeftBorder, Color.White);
 
                 //draw right border
+                spriteBatch.Draw(hoverCardSprite, new Rectangle(hoverCardLocations[i].X + hoverCardLocations[i].Width, hoverCardLocations[i].Y, 10, hoverCardLocations[i].Height), hoverCardRightBorder, Color.White);
+
             }
 
         }
