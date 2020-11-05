8b18e851b6c161a8449c5989edc1c26932a5ae8f
Steve Sefchick <stevesefchick@gmail.com>
2020-11-04 19:23:42 -0500

    Create UIWindow class

---
+++ UIWindow.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace The_Mark
+{
+    class UIWindow
+    {
+    }
+}
