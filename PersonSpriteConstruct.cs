cf103abab2c21410434bf1e31c8e173cfaf4d5a8
stevesefchick <stevesefchick@gmail.com>
2020-12-20 21:56:15 -0500

    started work on person sprite construct

---
+++ PersonSpriteConstruct.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace The_Mark
+{
+    class PersonSpriteConstruct
+    {
+        //enums
+        //Torso Part
+        //Feet Part
+        //Hands Part
+        //Head Part
+        //Hair Part
+        //Face Part
+
+
+        public PersonSpriteConstruct()
+        {
+
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+
+
+        }
+
+    }
+}
